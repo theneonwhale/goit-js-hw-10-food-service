1d0f1fb017424db61dde115d801d127d01f5c12a
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-09 23:28:57 +0300

    fix adding classes

--- src/js/theme.js
+++ src/js/theme.js
@@ -14,17 +14,23 @@ const isDarkTheme = localStorage.getItem(STORAGE_CHECKED) === 'true';
 
 if (isDarkTheme) {
     refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
     refs.checkbox.checked = true;
+} else {
+    refs.body.classList.add(Theme.LIGHT);
 }
 
 refs.checkbox.addEventListener('change', onThemeSwitch);
 
 function onThemeSwitch(evt) {
-    refs.body.classList.toggle(Theme.DARK);
-    refs.body.classList.toggle(Theme.LIGHT);
-
     const CHECKED = evt.srcElement.checked;
+
+    if (CHECKED) {
+        refs.body.classList.add(Theme.DARK);
+        refs.body.classList.remove(Theme.LIGHT);
+    } else {
+        refs.body.classList.add(Theme.LIGHT);
+        refs.body.classList.remove(Theme.DARK);
+    };
 
     localStorage.setItem(STORAGE_CHECKED, CHECKED);
 };
